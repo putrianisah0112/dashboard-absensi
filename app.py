11f313344c5cae3ffe78bf24a5255807dd6ab473
putrianisah0112 <putrianisah0112@gmail.com>
2026-01-30 21:03:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 # paste seluruh kode Streamlit kamu di bawah sini
+import streamlit as st
+import pandas as pd
+
+st.title("Dashboard Absensi Fisika")
+
+data = {
+    "Nama": ["Ani", "Budi", "Citra"],
+    "Status": ["Hadir", "Izin", "Hadir"]
+}
+
+df = pd.DataFrame(data)
+
+st.subheader("Data Absensi Siswa")
+st.dataframe(df)
